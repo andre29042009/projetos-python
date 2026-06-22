09b17607096b09fe833555c39bc6a02aa4438be3
Andre Garrido <andretrianaaa@gmail.com>
2026-06-21 22:15:28 -0300

    Quiz usando Programação Orientada a Objetos

---
+++ Quiz com OOP/data.py
@@ -0,0 +1,17 @@
+question_data = [
+    {"text": "Polvos possuem três corações.", "answer": "True"},
+    {"text": "Bananas crescem em árvores.", "answer": "False"},
+    {"text": "Um dia em Vênus dura mais do que um ano em Vênus.", "answer": "True"},
+    {"text": "O mel pode estragar se ficar guardado por muito tempo.", "answer": "False"},
+    {"text": "Os tubarões existiam antes das árvores.", "answer": "True"},
+    {"text": "O som não se propaga no espaço.", "answer": "True"},
+    {"text": "Os cavalos-marinhos machos são os responsáveis pela gestação dos filhotes.", "answer": "True"},
+    {"text": "O sangue de todos os animais é vermelho.", "answer": "False"},
+    {"text": "Existem mais estrelas no universo do que grãos de areia em todas as praias da Terra.", "answer": "True"},
+    {"text": "Os cangurus conseguem andar para trás normalmente.", "answer": "False"},
+    {"text": "A Torre Eiffel fica alguns centímetros mais alta durante o verão.", "answer": "True"},
+    {"text": "As impressões digitais dos coalas são muito parecidas com as dos seres humanos.", "answer": "True"},
+    {"text": "O coração de uma baleia-azul pode pesar mais de 100 quilos.", "answer": "True"},
+    {"text": "Relâmpagos nunca atingem o mesmo lugar duas vezes.", "answer": "False"},
+    {"text": "Água quente pode congelar mais rápido que água fria em algumas situações.", "answer": "True"}
+]
